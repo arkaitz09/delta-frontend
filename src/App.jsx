ac8d766f3b38cb1d8baa89fd9c0d842cc9602175
Arkaitz <arkaitz09@gmail.com>
2025-04-15 02:05:57 +0200

    Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div id="papi">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Zorionak mi amor!</h1>
+        <h2>28 añitos ❤️</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img id="aidaf" src="static\cumple-aida\Aida.jpg" alt="Aida" width="300" height="350" ></img>
+    </div>
+    <div className="card" id="content">
+      <h3>Esta es la presentación de mi regalito...</h3>
+      <p>Estarás pensando, uy, y mi regalo? Pues esque toda espera merece la pena.</p>
+      <p><a href="https://www.mozilla.org/es-ES/"> Pulsa aquí</a> para continuar.</p>
+    </div>
     </>
   )
 }
